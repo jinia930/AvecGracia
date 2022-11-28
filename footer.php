85ba67a61068b0db2e7cab437579840aa496f006
jinia930 <sadiar640@gmail.com>
2022-11-28 23:21:00 +0600

    first commit

---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="contact.php">Contact</a>
+            <a href="shop.php">Shop</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="login.php">Login</a>
+            <a href="register.php">Register</a>
+            <a href="orders.php">My orders</a>
+            <a href="cart.php">My cart</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <p> <i class="fas fa-phone"></i> +88012345678</p>
+            <p> <i class="fas fa-phone"></i> +88016768689 </p>
+            <p> <i class="fas fa-envelope"></i> jinia@gmail.com </p>
+            <p> <i class="fas fa-map-marker-alt"></i> Chittagong</p>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+        </div>
+
+    </div>
+
+    <div class="credit">Thanks for visiting,<?php?> Have a nice day!<span> By Jinia</span> </div>
+
+</section>
